ff5c3976f9e57645172fcf2a9b20dfa6860c94c3
Eleonora Nocentini Sköldebrink <eleonora.nocentini@gmail.com>
2024-02-04 09:13:59 +0100

    first push

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Middleware för att tolka JSON i förfrågningar
+app.use(express.json());
+
+// En grundläggande route som skickar ett svar när den får en GET-förfrågan till root ('/')
+app.get("/", (req, res) => {
+  res.send("Välkommen till min receptbok backend!");
+});
+
+// Starta servern så att den lyssnar på den angivna porten
+app.listen(port, () => {
+  console.log(`Servern körs på port ${port}`);
+});
