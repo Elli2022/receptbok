972b9b8baa5c32703de5af9cee6fdde8a4663721
Eleonora Nocentini Sköldebrink <eleonora.nocentini@gmail.com>
2024-02-04 09:38:11 +0100

    fixed server.js

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,24 @@
+require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 const port = process.env.PORT || 3001;
 
 // Middleware för att tolka JSON i förfrågningar
 app.use(express.json());
+
+// Anslut till MongoDB med Mongoose
+mongoose
+  .connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB Atlas");
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB", error);
+  });
 
 // En grundläggande route som skickar ett svar när den får en GET-förfrågan till root ('/')
 app.get("/", (req, res) => {
