fdf5e55bc4840072acb1485f27118172466a9b77
Eleonora Nocentini Sköldebrink <eleonora.nocentini@gmail.com>
2024-02-12 20:40:19 +0100

    installed ts on backend AND added portions on models for recipeScheme

--- backend/models/Recipe.ts
+++ backend/models/Recipe.ts
@@ -1,24 +1,30 @@
-// backend/models/Recipe.ts
 import mongoose, { Schema, Document } from "mongoose";
 
 interface IRecipe extends Document {
   name: string;
   description?: string;
+  portions?: number;
   ingredients: string[];
-  instructions?: string;
+  instructions?: string[];
   createdAt: Date;
 }
 
 const recipeSchema: Schema = new Schema({
   name: { type: String, required: true },
   description: { type: String },
+  portions: { type: Number },
   ingredients: { type: [String], required: true },
-  instructions: { type: String },
+  instructions: { type: [String] },
   createdAt: { type: Date, default: Date.now },
 });
 
 // Skapa ett textindex
-recipeSchema.index({ name: "text", description: "text", ingredients: "text" });
+recipeSchema.index({
+  name: "text",
+  description: "text",
+  ingredients: "text",
+  instructions: "text",
+});
 
 const Recipe = mongoose.model<IRecipe>("Recipe", recipeSchema);
 
--- backend/server.ts
+++ backend/server.ts
@@ -1,8 +1,13 @@
 //backend/server.ts
 // dotenv kongifuarion i toppen av filen för att säkerställa att miljövariablerna från .env laddas innan de används
-require("dotenv").config(); // Laddar miljövariabler från .env-filen
-const express = require("express");
-const mongoose = require("mongoose");
+// Importera nödvändiga moduler och typer
+import dotenv from "dotenv";
+import express, { Request, Response } from "express";
+import mongoose from "mongoose";
+
+// Konfigurera dotenv för att ladda miljövariabler från .env-filen
+dotenv.config();
+
 const app = express();
 const port = process.env.PORT || 3001;
 
@@ -10,17 +15,17 @@ app.use(express.json()); // Middleware för att tolka JSON
 
 // Anslut till MongoDB med Mongoose
 mongoose
-  .connect(process.env.DATABASE_URL)
+  .connect(process.env.DATABASE_URL!)
   .then(() => console.log("Connected to MongoDB Atlas"))
-  .catch((error) => console.error("Error connecting to MongoDB", error));
+  .catch((error: any) => console.error("Error connecting to MongoDB", error));
 
 // Grundläggande välkomstmeddelande
-app.get("/", (req, res) => {
+app.get("/", (req: Request, res: Response) => {
   res.send("Välkommen till min receptbok backend!");
 });
 
 // Importera och använd recept-routes
-const recipesRouter = require("../backend/routes/recipe");
+import recipesRouter from "./routes/recipe"; // Ändra './routes/recipe' till korrekt sökväg baserat på din filstruktur
 app.use("/recipes", recipesRouter);
 
 // Starta servern
