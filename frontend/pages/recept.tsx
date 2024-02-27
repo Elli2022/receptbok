201a1942531b183ecc3d8a4848d708e3775e9c33
Eleonora Nocentini Sköldebrink <eleonora.nocentini@gmail.com>
2024-02-27 20:12:24 +0100

    added search field on front end for searching for recipes

--- frontend/pages/recept.tsx
+++ frontend/pages/recept.tsx
@@ -1,5 +1,4 @@
-//pages/recept.tsx
-import React, { ReactNode } from "react";
+import React, { ReactNode, useEffect, useState } from "react";
 import Navbar from "@/app/components/Navbar";
 import Footer from "@/app/components/Footer";
 import Link from "next/link";
@@ -29,23 +28,48 @@ export async function getServerSideProps() {
   };
 }
 
-// Komponent för att visa enskilda recept
-const ReceptPage = ({ recept }: { recept: Recept[] }) => {
+const ReceptPage = ({ recept }: Props) => {
+  // Söktillstånd
+  const [searchTerm, setSearchTerm] = useState("");
+  const [filteredRecept, setFilteredRecept] = useState<Recept[]>([]);
+
+  // Uppdatera filtrerade recept baserat på sökterm
+  useEffect(() => {
+    if (recept) {
+      const result = recept.filter((receptItem) =>
+        receptItem.name.toLowerCase().includes(searchTerm.toLowerCase())
+      );
+      setFilteredRecept(result);
+    }
+  }, [searchTerm, recept]);
+
   return (
     <div className="max-w-8xl mx-auto px-4 py-8">
       <Navbar />
       <h1 className="text-4xl font-bold text-center mb-8">Recept</h1>
+      {/* Sökfält */}
+      <input
+        type="text"
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+        placeholder="Sök efter recept..."
+        className="mb-4 p-2 w-full text-black"
+      />
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-        {recept.map((recept) => (
-          <Link key={recept._id} href={`/recept/${recept._id}`} passHref>
+        {filteredRecept.map((receptItem) => (
+          <Link
+            key={receptItem._id}
+            href={`/recept/${receptItem._id}`}
+            passHref
+          >
             <div className="text-black bg-white rounded-lg overflow-hidden shadow-lg hover:shadow-xl transition-shadow duration-300 ease-in-out cursor-pointer">
               <img
-                src={recept.image}
-                alt={recept.name}
+                src={receptItem.image}
+                alt={receptItem.name}
                 className="w-full h-64 object-cover"
               />
               <div className="p-6">
-                <h2 className="text-2xl font-bold mb-2">{recept.name}</h2>
+                <h2 className="text-2xl font-bold mb-2">{receptItem.name}</h2>
               </div>
             </div>
           </Link>
