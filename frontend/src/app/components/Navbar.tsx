c55920171c0fb8f6ea925794c78eac87f6d57633
Eleonora Nocentini Sköldebrink <eleonora.nocentini@gmail.com>
2024-02-29 17:36:21 +0100

    push

--- frontend/src/app/components/Navbar.tsx
+++ frontend/src/app/components/Navbar.tsx
@@ -5,25 +5,27 @@ import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <nav>
-      <ul style={{ listStyleType: "none", padding: 0 }}>
-        <li style={{ display: "inline", marginRight: "20px" }}>
-          <Link href="/" style={{ textDecoration: "none" }}>
-            Hem
-          </Link>
-        </li>
-        <li style={{ display: "inline", marginRight: "20px" }}>
-          <Link href="/recept" style={{ textDecoration: "none" }}>
-            Recept
-          </Link>
-        </li>
-        <li style={{ display: "inline" }}>
-          <Link href="/about" style={{ textDecoration: "none" }}>
-            Om Mig
-          </Link>
-        </li>
-      </ul>
-    </nav>
+    <>
+      <nav>
+        <ul style={{ listStyleType: "none", padding: 0 }}>
+          <li style={{ display: "inline", marginRight: "20px" }}>
+            <Link href="/" style={{ textDecoration: "none" }}>
+              Hem
+            </Link>
+          </li>
+          <li style={{ display: "inline", marginRight: "20px" }}>
+            <Link href="/recept" style={{ textDecoration: "none" }}>
+              Recept
+            </Link>
+          </li>
+          <li style={{ display: "inline" }}>
+            <Link href="/about" style={{ textDecoration: "none" }}>
+              Om Mig
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </>
   );
 };
 
