f858b9b2ab65e972fef430135ebc4356ddcfa366
Eleonora Nocentini Sköldebrink <eleonora.nocentini@gmail.com>
2024-02-29 16:40:02 +0100

    updated AboutPage

--- frontend/pages/about.tsx
+++ frontend/pages/about.tsx
@@ -1,16 +1,27 @@
-//pages/about.tsx
-import React, { ReactNode } from "react";
+// pages/about.tsx
+import React from "react";
 import Navbar from "@/app/components/Navbar";
 import Footer from "@/app/components/Footer";
 import Link from "next/link";
 
-export default function aboutPage() {
-  return <Navbar />;
-  <p>
-    Jag ville skapa en receptblogg där stegen var utförliga, lättlästa och där
-    ingredienserna kommer i den ordningen som de ska användas i instruktionerna.
-    Idéen kom från att jag tyckte att det var jobbigt att ha mian gluten- och
-    nötfria recept samlade både på recept.se, instagram, facebook, en receptbok
-    i pappersformat etc.
-  </p>;
-}
+const AboutPage = () => {
+  return (
+    <>
+      <Navbar />
+      <div className="container mx-auto px-4 py-8">
+        <h1 className="text-2xl font-bold mb-4">Om Oss</h1>
+        <p>
+          Jag ville skapa en receptblogg där stegen var utförliga, lättlästa och
+          där ingredienserna kommer i den ordningen som de ska användas i
+          instruktionerna. Idéen kom från att jag tyckte att det var jobbigt att
+          ha mina gluten- och nötfria recept samlade både på recept.se,
+          instagram, facebook, en receptbok i pappersformat etc.
+        </p>
+        {/* Andra innehållselement här */}
+      </div>
+      <Footer />
+    </>
+  );
+};
+
+export default AboutPage;
